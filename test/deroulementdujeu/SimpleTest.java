5e8ec434c96b7e54f9bb0e3eaa9dd063d70d2c46
VincentChailley <vchailley@gmail.com>
2016-04-03 22:19:22 +0200

    Ajouts de test JUnit (dossier test)

--- test/deroulementdujeu/SimpleTest.java
+++ test/deroulementdujeu/SimpleTest.java
@@ -11,17 +11,20 @@ public class SimpleTest {
 		Piece tetrominos = new Piece();
 		Piece result = tetrominos.rotate();
 		Assert.assertNotSame("La pièce est différente puisqu'elle à tourné",result);
+		//La pièce effectue-t-elle bien une rotation ?
 	}
 	@Test
 	public void testPiecegodown(){
 		Piece tetrominos = new Piece();
 		Piece result = tetrominos.goDown();
 		Assert.assertNotSame("L'emplacement de la pièce doit avoir changé",result);
+		//La pièce est-elle bien descendu d'un cran ?
 	}
-	/*@Test
-	public void testrotatePiece(){
-		Piece tetrominos = new Piece();
-		Piece result = tetrominos.rotate();
-		Assert.assertNotSame("La pièce est différente puisqu'elle à tourné",result);
-	}*/
+	@Test
+	public void testrandomPiece(){
+		Piece tetromino1 = new Piece();
+		Piece tetromino2 = new Piece();
+		Assert.assertNotEquals(tetromino1, tetromino2);
+		//Les pièces sortent-elle de façon aléatoire ?
+	}
 }
